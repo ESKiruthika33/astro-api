a8ba4a93d3f73cdc81de8fbf361461b516b045f6
ESKiruthika33 <kiruthika.devops33@gmail.com>
2026-06-06 17:28:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,12 +7,6 @@
 const express      = require("express");
 const cors         = require("cors");
 const { DateTime } = require("luxon");
-const solar        = require("astronomia/solar");
-const moonposition = require("astronomia/moonposition");
-const planetposition = require("astronomia/planetposition");
-const { JulianDay } = require("astronomia/julian");
-const sidereal     = require("astronomia/sidereal");
-const coord        = require("astronomia/coord");
 
 const app  = express();
 const PORT = process.env.PORT || 3001;
